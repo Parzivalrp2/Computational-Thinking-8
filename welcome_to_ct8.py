800182062a33e89580e080a26a82be37685257f5
Parzivalrp2 <amithendrickson@seattleacademy.org>
2026-03-09 21:49:11 +0000

    commit

--- welcome_to_ct8.py
+++ welcome_to_ct8.py
@@ -27,4 +27,6 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print(name+" is bored.")
+print(name+" prefers micro")
+print(name+" prefers vi")        
