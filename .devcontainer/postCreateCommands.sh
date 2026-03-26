1787b4593adbcae63c6cbfa1de0e1b724a2ad19b
Parzivalrp2 <amithendrickson@seattleacademy.org>
2026-03-26 21:33:36 +0000

    commit

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python3-tk
-sudo apt install python-is-python3
+apt-get update
+apt-get install -y python3-pip
+apt-get install -y python3-tk
+apt install python-is-python3
 pip3 install --user -r requirements.txt
---
+++ converse.py
@@ -0,0 +1,41 @@
+# Section 1 - Your code
+from utils import *
+
+set_background("moon")
+
+s1 = create_sprite("alien", -200, 0)
+s2 = create_sprite("cat", 200, 0)
+
+s1.color("green")
+s2.color("orange")
+time.sleep(5)
+
+s1.write("Where are we?",font = ("Arial", 20, "normal"))
+window.update()
+time.sleep(1)
+
+s1.clear()
+window.update()
+time.sleep(1)
+
+s2.write("On the moon!",font = ("Arial", 20, "normal"))
+window.update()
+time.sleep(1)
+
+s2.clear()
+window.update()
+time.sleep(1)
+
+s1.write(f"I'm looking for ",font = ("Arial", 20, "normal"))
+window.update()
+time.sleep(1)
+
+s1.clear()
+s1.write("Have you seen them?",font = ("Arial", 20, "normal"))
+window.update()
+time.sleep(1)
+
+######################################################################
+# Section 2 - Keeping the window open (DON'T CHANGE!!)
+window.update()
+turtle.exitonclick()
