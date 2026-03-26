6b9613bf703a0f2793c8f5485f53a6a43f1835b6
Parzivalrp2 <amithendrickson@seattleacademy.org>
2026-03-26 21:58:49 +0000

    commit

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,5 +1,6 @@
-apt-get update
-apt-get install -y python3-pip
-apt-get install -y python3-tk
-apt install python-is-python3
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-tk
+sudo apt-get install python-is-python3
+sudo DEBIAN_FRONTEND=noninteractive && apt-get install -y kde-full
 pip3 install --user -r requirements.txt
