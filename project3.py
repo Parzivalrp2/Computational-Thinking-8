9db1763e654f2bab78de6bd5a9582b991d8c9699
Parzivalrp2 <amithendrickson@seattleacademy.org>
2026-04-06 19:17:34 +0000

    commit

--- project3.py
+++ project3.py
@@ -2,16 +2,16 @@
 from utils import *
 
 # Section 1 - Variables
-x1 = 0
+x1 = 200
 y1 = 200
 
-x2 = 0
+x2 = 200
 y2 = 100
 
-x3 = 0
+x3 = 200
 y3 = 0
 
-x4 = 0
+x4 = 200
 y4 = -100
 
 
@@ -24,15 +24,20 @@ t2 = create_sprite("horse2",x2,y2)
 t3 = create_sprite("horse3",x3,y3)
 t4 = create_sprite("horse4",x4,y4)
 
+one_finished = False
+two_finished = False
+three_finished = False
+four_finished = False
 
 # # Section 3 - Racing
 i2 = 0
+start_time = time.time_ns()
 for i in range(100):
     if i == i2 + 10 or i == 0:
-        speed1 = random.randint(5,15)
-        speed2 = random.randint(5,15)
-        speed3 = random.randint(5,15)
-        speed4 = random.randint(5,15)
+        speed1 = -random.randint(5,15)
+        speed2 = -random.randint(5,15)
+        speed3 = -random.randint(5,15)
+        speed4 = -random.randint(5,15)
         i2 = i
     x1 += speed1
     x2 += speed2
@@ -43,10 +48,24 @@ for i in range(100):
     t2.goto(x2, y2)
     t3.goto(x3, y3)
     t4.goto(x4, y4)
-
     window.update()
+    if x1 <= -400:
+        one_finished = True
+        one_time = time.time_ns()
+    if x2 <= -400:
+        two_finished = True
+        two_time = time.time_ns()
+    if x3 <= -400:
+        three_finished = True
+        three_time = time.time_ns()
+    if x4 <= -400:
+        four_finished = True
+        four_time = time.time_ns()
+    if one_finished and two_finished and three_finished and four_finished:
+        break
     time.sleep(0.1)
 
+print(x1,x2,x3,x4)
 
 # # Section 4 - Winner
 # # TODO - complete the elif for player 2 winning
